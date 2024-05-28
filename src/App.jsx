c40894c54e1f88fa9ddb1eb35b50be74c047fd2a
rahathossain3 <rahathossain210@gmail.com>
2024-05-28 20:30:20 +0600

    first commit without eletricity

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex gap-4">
+      <div className="p-10 border border-red-500">01</div>
+      <div className="p-10 border border-red-500">02</div>
+      <div className="p-10 border border-red-500">03</div>
+    </div>
+  );
+}
+
+export default App;
